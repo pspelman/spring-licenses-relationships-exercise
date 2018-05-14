544f9191c59f268b22b9361e98759677d69a104d
Phil Spelman <phil_spelman@hotmail.com>
2018-05-14 12:13:29 -0700

    First commit: Empty project

---
+++ src/main/java/com/philspelman/springlicensesrelationshipsexercise/SpringLicensesRelationshipsExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.philspelman.springlicensesrelationshipsexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLicensesRelationshipsExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLicensesRelationshipsExerciseApplication.class, args);
+    }
+}
